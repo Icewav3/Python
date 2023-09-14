66e13a2ae9058d7654b2c7e32bc11ab44480f1d0
Icewav3 <coledorman12@gmail.com>
2023-09-14 10:03:23 -0500

    Initial commit

---
+++ Helloworld.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+print("\n")
+
+var = str("1")
+print( int == type(int(var)))
+print("\n")
+
+truncate_test = float(8.9923)
+print(int(truncate_test))
+print("\n")
+
+print(round(truncate_test))
+print("\n")
+
+
+myint = 1
+myfloat = float(myint)
+mystring = str(myfloat)
+print(mystring)
+print(type(myint), type(myfloat), type(mystring))
+
---
+++ datastoring test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 14 09:34:59 2023
+
+@author: coled
+"""
+
+mydata = []
+
+read = input("input a position: ")
+read.parse input = "31, 1"
+break into 31 and 1
