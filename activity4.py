3fdd520e911baf2f9470a58e3b855727eeab84c2
Icewav3 <coledorman12@gmail.com>
2023-09-21 12:28:56 -0500

    updat

---
+++ activity4.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 12:22:55 2023
+
+@author: coled
+"""
+
+prefix = str("The temperature on, ")
+day = str("Monday")
+temp = float(-13.4)
+day2 = str("Tuesday")
+temp2 = int(7)
+
+
+print("The temperature on, "+day+" was "+str(temp))
+print(f"The temperature on, {day2} was {temp2}")
